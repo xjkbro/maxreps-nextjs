7794253d53ef0dc4d94aefe7d5acda0c58b943e4
Jason-Kyle De Lara <jason8oh8@gmail.com>
2023-03-09 10:33:35 -0800

    maxreps init w capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'maxreps-nextjs',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
